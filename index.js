76e67436f5a085f519a3880f931cc74a209c45d9
Duc Vu <de.yd315@gmail.com>
2019-04-23 04:47:50 -0500

    set up the server succesfully

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // implement your API here
 
-// 
+const express = require('express');
+
+const DB = require('./data/db.js')
+
+const server = express();
+
+
+
+server.listen(5000, () => {
+    console.log('\n*** Server Running on http://localhost:5000 ***\n');
+  });
